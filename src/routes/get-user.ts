81c37b14b7ee0bc7800c7e9c34cfb053e4c1cb8e
jules marchand <friarobaz@gmail.com>
2021-10-27 14:39:00 +0200

    reset

--- src/routes/get-user.ts
+++ src/routes/get-user.ts
@@ -6,37 +6,24 @@ export const handler: Handler = async (event, context) => {
   if (!isPasswordOk(event)) {
     return { statusCode: 401, body: "Unauthorized" }
   }
-  const headers = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers": "Content-Type",
-    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-  }
 
-  /* if (!userId) {
+  const userId = event.queryStringParameters?.id
+
+  if (!userId) {
     return {
       statusCode: 404,
       body: "Parameter `id` missing",
     }
-  } */
-  //########################################################
-  if (event.httpMethod === "OPTIONS") {
-    return {
-      statusCode: 200,
-      headers,
-      body: JSON.stringify({ message: "Successful preflight call." }),
-    }
-  } else if (event.httpMethod === "POST") {
-    const userId = event.queryStringParameters?.id
-    const client = await getClient()
-    const auth = await getAuth(client)
-    const user = await getUser(client, auth, userId)
-    return {
-      statusCode: 200,
-      headers,
-      body: JSON.stringify({
-        user,
-      }),
-    }
   }
-  //########################################################
+
+  const client = await getClient()
+  const auth = await getAuth(client)
+  const user = await getUser(client, auth, userId)
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      user,
+    }),
+  }
 }
