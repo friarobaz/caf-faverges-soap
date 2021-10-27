eecf961491c501413eaab547bd0f3372308afed0
jules marchand <friarobaz@gmail.com>
2021-10-27 15:34:10 +0200

    setup firebase

--- index.js
+++ index.js
@@ -23,3 +23,12 @@ const getClub = async () => {
   const data = await response.json()
   console.log(data.club)
 }
+
+db.collection("users")
+  .add({ couleur: "rouge" })
+  .then((docRef) => {
+    console.log("Document written with ID: ", docRef.id)
+  })
+  .catch((error) => {
+    console.error("Error adding document: ", error)
+  })
