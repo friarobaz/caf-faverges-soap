b441f9fd03152800c929bd68a255bc8bce5f6ada
jules marchand <friarobaz@gmail.com>
2021-10-27 14:09:42 +0200

    test get users with headers

--- src/routes/get-user.ts
+++ src/routes/get-user.ts
@@ -1,29 +1,44 @@
-import { getClient, getAuth, getUser } from '../soap';
-import { Handler } from '@netlify/functions'
-import { isPasswordOk } from '../auth';
+import { getClient, getAuth, getUser } from "../soap"
+import { Handler } from "@netlify/functions"
+import { isPasswordOk } from "../auth"
 
 export const handler: Handler = async (event, context) => {
   if (!isPasswordOk(event)) {
-    return {statusCode: 401, body: 'Unauthorized'}
+    return { statusCode: 401, body: "Unauthorized" }
+  }
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
   }
 
-  const userId = event.queryStringParameters?.id
-
-  if (!userId) {
+  /* if (!userId) {
     return {
       statusCode: 404,
-      body: 'Parameter `id` missing'
+      body: "Parameter `id` missing",
+    }
+  } */
+  //########################################################
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify({ message: "Successful preflight call." }),
+    }
+  } else if (event.httpMethod === "POST") {
+    const userId = event.queryStringParameters?.id
+    const client = await getClient()
+    const auth = await getAuth(client)
+    const user = await getUser(client, auth, userId)
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify({
+        user,
+      }),
     }
   }
-  
-  const client = await getClient();
-  const auth = await getAuth(client);
-  const user = await getUser(client, auth, userId);
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      user,
-    }),
-  }
+  //########################################################
 }
+
+exports.handler = async (event) => {}
