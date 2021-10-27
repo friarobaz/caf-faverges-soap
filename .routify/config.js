95b713066c2d9a3284ba6e1b4b34b2839bc8027f
jules marchand <friarobaz@gmail.com>
2021-10-27 15:55:46 +0200

    routify mess

---
+++ .routify/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "pages": "src/pages",
+  "sourceDir": "public",
+  "routifyDir": ".routify",
+  "ignore": "",
+  "dynamicImports": true,
+  "singleBuild": false,
+  "noHashScroll": false,
+  "distDir": "dist",
+  "hashScroll": true,
+  "extensions": [
+    "html",
+    "svelte",
+    "md",
+    "svx"
+  ],
+  "started": "2021-10-27T13:49:17.861Z"
+}
